45b9747f60981795720e9da6f29244b254542b26
Willy <11148913+willyfromtheblock@users.noreply.github.com>
2022-08-11 10:34:05 +0200

    better error handling and formatting

--- index.js
+++ index.js
@@ -1,54 +1,68 @@
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event))
-})
+addEventListener("fetch", (event) => {
+  event.respondWith(handleRequest(event));
+});
 
- async function handleRequest(event) {
-  const request = event.request; 
+async function handleRequest(event) {
+  const request = event.request;
 
   async function readRequestBody(request) {
-    const { headers } = request
-    const contentType = headers.get("content-type") || ""
-  
+    const { headers } = request;
+    const contentType = headers.get("content-type") || "";
+
     if (contentType.includes("application/json")) {
-      return request.json()
+      return request.json();
     }
   }
 
   async function parseBody(reqBody) {
-    let foundDifference = false; 
+    let foundDifference = false;
     let answerArray = [];
-    if(reqBody.hasOwnProperty("coin") && reqBody.hasOwnProperty("addresses")) {
-        //valid request
-        let kvInstance; 
-        if(reqBody.coin === "peercoin") {
-          kvInstance = peercoin
-        } else if(reqBody.coin === "peercoinTestnet") {
-          kvInstance = peercoinTestnet;
-        }
+    if (
+      reqBody !== undefined &&
+      reqBody.hasOwnProperty("coin") &&
+      reqBody.hasOwnProperty("addresses")
+    ) {
+      //valid request
+      let kvInstance;
+      if (reqBody.coin === "peercoin") {
+        kvInstance = peercoin;
+      } else if (reqBody.coin === "peercoinTestnet") {
+        kvInstance = peercoinTestnet;
+      } else {
+        return { status: 400 };
+      }
 
-      for(const element of reqBody.addresses) {
+      for (const element of reqBody.addresses) {
         for (var key of Object.keys(element)) {
           const addr = key;
           const number = element[key];
           //get data from database
-          const numberInKV = await kvInstance.get(addr)
-          if(numberInKV > number) {
-            foundDifference = true; 
-            answerArray.push({address: addr, tx: parseInt(numberInKV)});
+          const numberInKV = await kvInstance.get(addr);
+          if (numberInKV > number) {
+            foundDifference = true;
+            answerArray.push({ address: addr, tx: parseInt(numberInKV) });
           }
-        }        
+        }
       }
     } else {
-      return new Response("bad request", {status: 400});
-   }
+      return { status: 400 };
+    }
 
-   return JSON.stringify({'foundDifference': foundDifference,'addresses': answerArray}, null, 2);
+    return JSON.stringify(
+      { foundDifference: foundDifference, addresses: answerArray },
+      null,
+      2
+    );
   }
 
-  const reqBody = await readRequestBody(request)
-  const parseResult = await parseBody(reqBody)
+  const reqBody = await readRequestBody(request);
+  const parseResult = await parseBody(reqBody);
+
+  if (parseResult.hasOwnProperty("status") && parseResult.status == 400) {
+    return new Response("bad request", { status: 400 });
+  }
 
   return new Response(parseResult, {
-    headers: { 'content-type': 'application/json' },
-  })
+    headers: { "content-type": "application/json" },
+  });
 }
